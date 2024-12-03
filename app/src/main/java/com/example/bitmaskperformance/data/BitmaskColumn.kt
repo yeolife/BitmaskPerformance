ed37936fc9558d60d2e6d324a9b0c886ddb6a2fe
yeolife <yeo2507@naver.com>
2024-12-04 02:41:58 +0900

    비트마스킹

--- app/src/main/java/com/example/bitmaskperformance/data/BitmaskColumn.kt
+++ app/src/main/java/com/example/bitmaskperformance/data/BitmaskColumn.kt
@@ -11,18 +11,34 @@ import kotlin.reflect.full.memberProperties
 annotation class BitPosition(val position: Int)
 
 // 바뀐 컬럼 비트 켜기
-fun <T : Any> bitmaskColumn(prevBit: Long, dto1: T, dto2: T): Long {
-    return dto1::class.memberProperties
-        .filter { it.findAnnotation<BitPosition>() != null }
-        .fold(prevBit) { newBit, prop ->
-            val bitPosition = prop.findAnnotation<BitPosition>()!!.position
-            val typedProp = prop as KProperty1<T, *>
-            val value1 = typedProp.get(dto1)
-            val value2 = typedProp.get(dto2)
+fun bitmaskColumn(prevBit: Long, dto1: CardEntity, dto2: CardEntity): Long {
+    var newBit = prevBit
+    val fields = CardEntity::class.memberProperties
+        .filter { it.name.startsWith("col") }
 
-            if (value1 != value2) newBit or (1L shl bitPosition) else newBit
+    fields.forEachIndexed { index, prop ->
+        val value1 = prop.get(dto1) as Int?
+        val value2 = prop.get(dto2) as Int?
+        if (value1 != value2) {
+            newBit = newBit or (1L shl index)
         }
+    }
+
+    return newBit
 }
+
+//fun <T : Any> bitmaskColumn(prevBit: Long, dto1: T, dto2: T): Long {
+//    return dto1::class.memberProperties
+//        .filter { it.findAnnotation<BitPosition>() != null }
+//        .fold(prevBit) { newBit, prop ->
+//            val bitPosition = prop.findAnnotation<BitPosition>()!!.position
+//            val typedProp = prop as KProperty1<T, *>
+//            val value1 = typedProp.get(dto1)
+//            val value2 = typedProp.get(dto2)
+//
+//            if (value1 != value2) newBit or (1L shl bitPosition) else newBit
+//        }
+//}
 
 // 바뀌지 않은 컬럼은 null
 fun <T : Any> getBitmaskDto(columnUpdate: Long, dto: T): T {

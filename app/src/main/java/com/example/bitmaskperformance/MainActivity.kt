49d03d595bbcfbb1680b9340dc04f85d0c369747
yeolife <yeo2507@naver.com>
2024-12-11 12:50:15 +0900

    비트마스킹

--- app/src/main/java/com/example/bitmaskperformance/MainActivity.kt
+++ app/src/main/java/com/example/bitmaskperformance/MainActivity.kt
@@ -49,10 +49,10 @@ fun Greeting(viewModel: CardViewModel, modifier: Modifier = Modifier) {
                         modifier = Modifier.padding(end = 8.dp),
                         horizontalArrangement = Arrangement.spacedBy(8.dp)
                     ) {
-                        Button(onClick = { viewModel.insertCards(10000) }) {
+                        Button(onClick = { viewModel.insertCards() }) {
                             Text("Add")
                         }
-                        Button(onClick = { viewModel.updateCards(10000) }) {
+                        Button(onClick = { viewModel.updateCards() }) {
                             Text("Update")
                         }
                         Button(onClick = { viewModel.deleteCards() }) {

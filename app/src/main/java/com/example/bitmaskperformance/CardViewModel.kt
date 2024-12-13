9c2bdd51bd993a1c0927c7dbcf1b11438b73e071
yeolife <yeo2507@naver.com>
2024-12-13 15:51:49 +0900

    비트마스킹

--- app/src/main/java/com/example/bitmaskperformance/CardViewModel.kt
+++ app/src/main/java/com/example/bitmaskperformance/CardViewModel.kt
@@ -134,6 +134,8 @@ class CardViewModel(application: Application): AndroidViewModel(application) {
 
             block()
 
+            System.gc()
+
             val endMemory = logMemoryUsage()
             val endTime = System.nanoTime()
 
